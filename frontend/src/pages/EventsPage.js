4e8f43f1b559f67a4b00fb4c6506297e3c7b2591
mojahid-belaman <mojahidbelaman@gmail.com>
2023-07-06 19:13:26 +0100

    hundle error with loader

--- frontend/src/pages/EventsPage.js
+++ frontend/src/pages/EventsPage.js
@@ -2,19 +2,23 @@ import { useLoaderData } from "react-router-dom";
 import EventsList from "../components/EventsList";
 
 function EventsPage() {
-  const events = useLoaderData();
+  const data = useLoaderData();
+  // if (data.isError) {
+  //   return <p>{data.message}</p>;
+  // }
+  const events = data.events;
   return <EventsList events={events} />;
 }
 
 export default EventsPage;
 
 export async function loader() {
-  const response = await fetch("http://localhost:8080/events");
+  const response = await fetch("http://localhost:8080/eventsdfsfs");
 
   if (!response.ok) {
-    // ...
+    // return { isError: true, message: "Could not fetch events." };
+    throw { status: 400, message: "Could not fetch events." };
   } else {
-    const resData = await response.json();
-    return resData.events;
+    return response;
   }
 }
