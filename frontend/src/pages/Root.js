4ec1f206e7b5775ecd68976dc1d47043754f790a
mojahid-belaman <mojahidbelaman@gmail.com>
2023-07-10 19:11:31 +0100

    delete some comments

--- frontend/src/pages/Root.js
+++ frontend/src/pages/Root.js
@@ -3,10 +3,12 @@ import MainNavigation from "../components/MainNavigation";
 import { Outlet } from "react-router-dom";
 
 const RootLayout = () => {
+  // const navigation = useNavigation();
   return (
     <>
       <MainNavigation />
       <main style={{ maxWidth: "60rem", margin: "auto", padding: "0 2rem" }}>
+        {/* {navigation.state === 'loading' && <p>LOADING...</p>} */}
         <Outlet />
       </main>
     </>
