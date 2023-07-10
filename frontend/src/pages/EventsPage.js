ad3c9f909e583d6592d6ebfff44b611c0a199b40
mojahid-belaman <mojahidbelaman@gmail.com>
2023-07-10 19:11:03 +0100

    delete some comments

--- frontend/src/pages/EventsPage.js
+++ frontend/src/pages/EventsPage.js
@@ -1,23 +1,18 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, json } from "react-router-dom";
 import EventsList from "../components/EventsList";
 
 function EventsPage() {
   const data = useLoaderData();
-  // if (data.isError) {
-  //   return <p>{data.message}</p>;
-  // }
-  const events = data.events;
-  return <EventsList events={events} />;
+  return <EventsList events={data.events} />;
 }
 
 export default EventsPage;
 
 export async function loader() {
-  const response = await fetch("http://localhost:8080/eventsdfsfs");
+  const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
-    // return { isError: true, message: "Could not fetch events." };
-    throw { status: 400, message: "Could not fetch events." };
+    throw json({ message: "Could not fetch events." }, { status: 500 });
   } else {
     return response;
   }
