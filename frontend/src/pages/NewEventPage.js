f67458028431f870949217fbc62aeb80f0170e10
mojahid-belaman <mojahidbelaman@gmail.com>
2023-07-11 18:49:56 +0100

    create action to save eveent in backend

--- frontend/src/pages/NewEventPage.js
+++ frontend/src/pages/NewEventPage.js
@@ -1,7 +1,33 @@
-import React from "react";
+import { json, redirect } from "react-router-dom";
+import EventForm from "../components/EventForm";
 
 const NewEventPage = () => {
-  return <h1>NewEventPage</h1>;
+  return <EventForm />;
 };
 
 export default NewEventPage;
+
+export async function action({ request, params }) {
+  const data = await request.formData();
+
+  const eventData = {
+    title: data.get("title"),
+    image: data.get("image"),
+    date: data.get("date"),
+    description: data.get("description"),
+  };
+
+  const response = await fetch("http://localhost:8080/events", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(eventData),
+  });
+  console.log(response);
+  if (!response.ok) {
+    throw json({ message: "Could not Save event." }, { status: 500 });
+  } else {
+    return redirect("/events");
+  }
+}
