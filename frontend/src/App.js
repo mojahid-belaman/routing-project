f9af620afa9cb284b836502a7d94aba9e91211f6
mojahid-belaman <mojahidbelaman@gmail.com>
2023-07-11 18:51:59 +0100

    add action to router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -26,7 +26,7 @@ import EventsPage, { loader as eventsLoader } from "./pages/EventsPage";
 import EventDetailPage, {
   loader as eventDetailLoader,
 } from "./pages/EventDetailPage";
-import NewEventPage from "./pages/NewEventPage";
+import NewEventPage, {action as newEventAction} from "./pages/NewEventPage";
 import EditEventPage from "./pages/EditEventPage";
 import RootLayout from "./pages/Root";
 import RootEventLayout from "./pages/RootEvent";
@@ -70,6 +70,7 @@ const router = createBrowserRouter([
           {
             path: "new",
             element: <NewEventPage />,
+            action: newEventAction
           },
         ],
       },
