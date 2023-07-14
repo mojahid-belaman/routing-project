fe9188e7da1b7a65dd7d1f302b8253622a7b0a77
mojahid-belaman <mojahidbelaman@gmail.com>
2023-07-14 20:33:28 +0100

    create loader to fetch events and use Await and Suspense

--- frontend/src/pages/EventsPage.js
+++ frontend/src/pages/EventsPage.js
@@ -1,19 +1,31 @@
-import { useLoaderData, json } from "react-router-dom";
+import { useLoaderData, json, defer, Await } from "react-router-dom";
 import EventsList from "../components/EventsList";
+import { Suspense } from "react";
 
 function EventsPage() {
-  const data = useLoaderData();
-  return <EventsList events={data.events} />;
+  const { events } = useLoaderData();
+  return (
+    <Suspense fallback={<p style={{ textAlign: "center" }}>LOADING...</p>}>
+      <Await resolve={events}>
+        {(loadedEvents) => <EventsList events={loadedEvents} />}
+      </Await>
+    </Suspense>
+  );
 }
 
 export default EventsPage;
 
-export async function loader() {
+async function loaderEvents() {
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
     throw json({ message: "Could not fetch events." }, { status: 500 });
   } else {
-    return response;
+    const data = await response.json();
+    return data.events;
   }
+}
+
+export function loader() {
+  return defer({ events: loaderEvents() });
 }
