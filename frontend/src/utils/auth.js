d50ee53125e5e3b4276345d7c334e0fa6cd139a4
mojahid-belaman <mojahidbelaman@gmail.com>
2023-07-20 20:11:38 +0100

    create function check token is exist or not yet

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,4 +1,14 @@
+import { redirect } from "react-router-dom";
+
 export const getToken = () => {
   const token = localStorage.getItem("token");
   return token;
 };
+
+export function checkAuthLoader() {
+  const token = getToken();
+  if (!token) {
+    return redirect("/auth");
+  }
+  return null;
+}
