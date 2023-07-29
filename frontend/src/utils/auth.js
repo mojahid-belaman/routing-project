7442484a6ccfa4ba32c6934cc1254cfb7bdf8048
mojahid-belaman <mojahidbelaman@gmail.com>
2023-07-29 17:41:02 +0100

    create function calculate duration expire token

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,9 +1,27 @@
 import { redirect } from "react-router-dom";
 
+export function getTokenDuration() {
+  const storeExpirDate = localStorage.getItem("expiration");
+  const expirDate = new Date(storeExpirDate);
+  const dateNow = new Date();
+  const duration = expirDate.getTime() - dateNow.getTime();
+  return duration;
+}
+
 export const getToken = () => {
   const token = localStorage.getItem("token");
+  if (!token) return null;
+  const tokenDuration = getTokenDuration();
+  if (tokenDuration < 0) return "EXPIRED";
   return token;
 };
+
+export function tokenDuration() {
+  const getTokenExpir = localStorage.getItem("expiration");
+  const tokenExpi = new Date(getTokenExpir).getMilliseconds();
+  const dateNowMilli = new Date().getMilliseconds();
+  return tokenExpi - dateNowMilli;
+}
 
 export function tokenLoader() {
   return getToken();
