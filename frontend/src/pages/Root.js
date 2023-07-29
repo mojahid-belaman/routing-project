a0b369dfc5cc593f079eefed789c54ab4147d4a3
mojahid-belaman <mojahidbelaman@gmail.com>
2023-07-29 17:41:58 +0100

    add useEffect to handle expiration token with setTimeout

--- frontend/src/pages/Root.js
+++ frontend/src/pages/Root.js
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
 import MainNavigation from "../components/MainNavigation";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLoaderData, useSubmit } from "react-router-dom";
+import { getTokenDuration } from "../utils/auth";
 
 const RootLayout = () => {
-  // const navigation = useNavigation();
+  const token = useLoaderData();
+  const submit = useSubmit();
+
+  useEffect(() => {
+    if (!token) return;
+
+    if (token === "EXPIRED") {
+      submit(null, { action: "/logout", method: "post" });
+      return;
+    }
+
+    const tokenDur = getTokenDuration();
+    console.log(tokenDur);
+
+    setTimeout(() => {
+      submit(null, { action: "/logout", method: "post" });
+    }, tokenDur);
+  }, [token, submit]);
   return (
     <>
       <MainNavigation />
       <main style={{ maxWidth: "60rem", margin: "auto", padding: "0 2rem" }}>
-        {/* {navigation.state === 'loading' && <p>LOADING...</p>} */}
         <Outlet />
       </main>
     </>
